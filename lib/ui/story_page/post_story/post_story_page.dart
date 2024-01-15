257ae4be7bb9c611f6a9832e0ab64d3528e6bf44
Afrizal Sebastian <sebastiangurning@gmail.com>
2024-01-15 22:26:26 +0700

    feat: get image from gallery and camera

--- lib/ui/story_page/post_story/post_story_page.dart
+++ lib/ui/story_page/post_story/post_story_page.dart
@@ -1,4 +1,11 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:provider/provider.dart';
+import 'package:story_app_flutter_intermediate/common/styles.dart';
+import 'package:story_app_flutter_intermediate/provider/post_story_provider.dart';
 
 class PostStory extends StatefulWidget {
   final BottomNavigationBar bottomNavigationBar;
@@ -13,8 +20,145 @@ class _PostStoryState extends State<PostStory> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Post Story'),
+      body: SafeArea(
+        child: ChangeNotifierProvider(
+          create: (_) => PostStoryProvider(),
+          child: SingleChildScrollView(
+            child: Consumer<PostStoryProvider>(
+              builder: (context, provider, _) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Container(
+                      color: Styles.primaryColor,
+                      padding: const EdgeInsets.all(16),
+                      child: const Row(
+                        children: [
+                          Text(
+                            'Post Story',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: 300,
+                      margin: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        color: Colors.white,
+                        border: Border.all(
+                          style: BorderStyle.solid,
+                          color: Colors.black26,
+                        ),
+                      ),
+                      child: provider.imagePath == null
+                          ? const Align(
+                              alignment: Alignment.center,
+                              child: Icon(
+                                Icons.image,
+                                size: 100,
+                              ),
+                            )
+                          : _showImage(provider),
+                    ),
+                    const SizedBox(height: 16),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          width: 100,
+                          child: TextButton(
+                            style: const ButtonStyle(
+                              backgroundColor: MaterialStatePropertyAll(
+                                Styles.primaryColor,
+                              ),
+                            ),
+                            onPressed: () => _onCameraView(provider),
+                            child: const Text(
+                              'Camera',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 28),
+                        SizedBox(
+                          width: 100,
+                          child: TextButton(
+                            style: const ButtonStyle(
+                              backgroundColor: MaterialStatePropertyAll(
+                                Styles.primaryColor,
+                              ),
+                            ),
+                            onPressed: () => _onGalleryView(provider),
+                            child: const Text(
+                              'Gallery',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                );
+              },
+            ),
+          ),
+        ),
+      ),
       bottomNavigationBar: widget.bottomNavigationBar,
     );
+  }
+
+  _onGalleryView(PostStoryProvider provider) async {
+    final isMacOS = defaultTargetPlatform == TargetPlatform.macOS;
+    final isLinux = defaultTargetPlatform == TargetPlatform.linux;
+    if (isMacOS || isLinux) return;
+
+    final picker = ImagePicker();
+
+    final pickedFile = await picker.pickImage(
+      source: ImageSource.gallery,
+    );
+
+    if (pickedFile != null) {
+      provider.setImageFile(pickedFile);
+      provider.setImagePath(pickedFile.path);
+    }
+  }
+
+  _onCameraView(PostStoryProvider provider) async {
+    final isAndroid = defaultTargetPlatform == TargetPlatform.android;
+    final isiOS = defaultTargetPlatform == TargetPlatform.iOS;
+    final isNotMobile = !(isAndroid || isiOS);
+    if (isNotMobile) return;
+
+    final picker = ImagePicker();
+
+    final pickedFile = await picker.pickImage(
+      source: ImageSource.camera,
+    );
+
+    if (pickedFile != null) {
+      provider.setImageFile(pickedFile);
+      provider.setImagePath(pickedFile.path);
+    }
+  }
+
+  Widget _showImage(PostStoryProvider provider) {
+    final imagePath = provider.imagePath;
+    return kIsWeb
+        ? Image.network(
+            imagePath.toString(),
+            fit: BoxFit.cover,
+          )
+        : Image.file(
+            File(imagePath.toString()),
+            fit: BoxFit.cover,
+          );
   }
 }
